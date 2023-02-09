9100414d6b5154945b8cfa3f3a5b162e036fd5dc
Jermaine Wates <jwates7@protonmail.com>
2023-02-08 17:53:38 -0700

    final commit

--- comparisons.rb
+++ comparisons.rb
@@ -18,30 +18,34 @@ string_teachers = "4"
 numberDogs = 0
 
 # EXAMPLE: print the result of the comparison: is number_seachers greater than number_students?
-puts "Is number_teachers greater than number_students?", number_teachers > number_students
+puts number_teachers > number_students
 # this should print: "Is numberTeachers greater than numberStudents?" false
 
 # YOU DO: print the result of the comparison: is number_teachers less than number_students?
 # this should print: true
-puts "Is the number of teachers less than the number of student", number_teachers < number_students
+puts  number_teachers < number_students
 
 # YOU DO: print the result of the comparison: is number_teachers equal to string_teachers?
 # this should print: false
-puts 
+puts number_teachers == string_teachers
 # YOU DO: print the result of the comparison: is number_teachers not equal to number_students?
 # this should print: true
+puts number_teachers != number_students
 
 # YOU DO: print the result of the comparison: is number_students greater than or equal to 20?
 # this should print: true
+puts number_students >= 20 
 
 # YOU DO: print the result of the comparison: is number_students greater than or equal to 21?
-# this should print: false
+# this should print: false 
+puts number_students >= 21
 
 # YOU DO: print the result of the comparison: is number_students less than or equal to 20?
 # this should print: true
-
+puts number_students <= 20 
 # YOU DO: print the result of the comparison: is number_students less than or equal to 21?
 # this should print: true
+puts number_students <= 21
 
 
 #-------------------
@@ -56,19 +60,27 @@ puts
 puts 4 < 9
 #YOU DO: Explain.
 
+puts = 'four is less than nine'
+
 books = 3
 puts 4 < books
 # YOU DO: Explain.
+
+puts = 'there are less books than 4'
 
 friends = 6
 siblings = 2
 puts friends > siblings
 # YOU DO: Explain.
 
+puts = 'there are more friends than siblings'
+
 attendees = 9
 meals = 8
 puts attendees != meals
 # YOU DO: Explain.
+
+puts = 'the amount of attendees does not equal the amoung of meals'
 
 
 #-------------------
@@ -91,15 +103,20 @@ age = 1
 
 # YOU DO:
 # Determine if the dog loves to play and loves treats
-
+puts loves_to_play && loves_treats
 
 # Determine if the dog loves to play and loves the dog park
 
+puts  loves_to_play || loves_dog_park
 
 # Determine if the dog loves to play or loves the dog park
 
+puts loves_to_play || loves_dog_park
 
 # Determine if the dog loves to play and is a puppy
 
+puts loves_to_play && age
+
 # What did your final line of code evaluate to? Why do you think that is? Explain.
 # ANSWER:
+ puts 'the final answer is one (1) and I think that is because that is the age of the dog (1) which is a puppy technically'
