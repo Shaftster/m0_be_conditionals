a22314df8697d0adfe80ce794ab767be930dcdd5
Jermaine Wates <jwates7@protonmail.com>
2023-02-14 18:34:19 -0700

    final commit

---
+++ mild.rb
@@ -0,0 +1,24 @@
+
+# ## Mild
+
+# Write a Ruby program that defines a variable 
+# that stores an Integer. The program should print 
+# out the String "even" if the Integer is even, and the 
+#S tring "odd" if the Integer is odd.
+
+
+grains_rice = 172083412
+    
+    if grains_rice.even?
+        puts 'even'
+    else grains_rice.odd?
+        puts 'odd'
+    end
+    
+    p grains_rice
+
+    
+ 
+
+    
+ 
